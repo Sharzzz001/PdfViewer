f0c1441f428be699bd57517425f9a61832c90db2
Sharzzz001 <sharanshetty.001@gmail.com>
2022-10-17 22:35:30 +0530

    Got file browser filenames and paths for sending data.

--- mobile/src/main/java/com/example/pdfviewer/Listofpdf.kt
+++ mobile/src/main/java/com/example/pdfviewer/Listofpdf.kt
@@ -1,10 +1,10 @@
 package com.example.pdfviewer
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
@@ -15,42 +15,54 @@ class Listofpdf : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listofpdf)
         val j: Uri? = intent.data;
+        val sendtowatch = findViewById<Button>(R.id.sendtowatch);
 //        Toast.makeText(this@Listofpdf,j,Toast.LENGTH_SHORT).show()
         Log.d("APath: ", j.toString())
 
         // important part
         val filenamesToDocumentFile = mutableMapOf<String, DocumentFile>()
+        val filepathsToDocumentFile = mutableMapOf<Uri, DocumentFile>()
         val documentsTree = j?.let { DocumentFile.fromTreeUri(this, it) } ?: return
         val childDocuments = documentsTree.listFiles()
         for (childDocument in childDocuments) {
-            childDocuments[0].name?.let {
+            childDocument.name?.let {
                 filenamesToDocumentFile[it] = childDocument
             }
+            childDocument.uri?.let {
+                filepathsToDocumentFile[it] = childDocument
+            }
         }
+
         val your_array_list: MutableList<String> = ArrayList()
+        val your_path_list: MutableList<Uri> = ArrayList()
+
 
         for(key in filenamesToDocumentFile.keys){
 //            Log.d("Key name:",key)
             your_array_list.add(key)
         }
-
-        val mylist= arrayOf(your_array_list);
-//        Log.d("mylist :", mylist.toString())
-//        Log.d("Child","Child: $mylist")
-        val finarr: MutableList<String> = ArrayList()
-        for(i in mylist){
-            val text = i.toString().replace("[", "").replace("]", "");
-//            Log.d("This is text: ", text)
-            finarr.add(text)
+        for(path in filepathsToDocumentFile.keys){
+//            Log.d("Key name:",key)
+            your_path_list.add(path)
         }
-
+        Log.d("path: ",your_path_list.toString())
+        Log.d("arr: ",your_array_list.toString())
 //        val a = j?.let { getFileName(it) }
 //        Log.d("Return Result: ",a.toString())
         val listview = findViewById<ListView>(R.id.ls)
         val arrayAdapter: ArrayAdapter<*>
         arrayAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, finarr)
+            android.R.layout.simple_list_item_1, your_array_list)
         listview.adapter = arrayAdapter
+        sendtowatch.setOnClickListener{
+            for(i in childDocuments){
+                Log.d("Child: ", i.toString())
+            }
+        }
+
+    }
+
+    private fun sendingdata(i: String) {
 
     }
 
